e17637f479655e8470f3980aedd6f21bf830f276
Roman Gascoin <54111474+Rgascoin@users.noreply.github.com>
2022-04-14 21:33:36 +0200

    Fix try new output file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'build',
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// const nextConfig = {
+//   distDir: 'build',
+//   reactStrictMode: true,
+// }
+//
+// module.exports = nextConfig
